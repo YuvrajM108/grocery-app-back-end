0d33a5010275dbde78e7ae48e68a62cafcd701d5
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-02-28 22:09:17 -0400

    Create login and logout functions in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: 'json'} do
       resources :sessions, only: [:create]
       resources :registrations, only: [:create]
+      delete :logout, to: 'sessions#logout'
+      get :logged_in to: 'sessions#logged_in'
       resources :items, only: [:index, :show, :update]
       get '/items/:id', to: 'items#show'
     end
