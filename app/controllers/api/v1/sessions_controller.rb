aa4d40668680ab95af1da5e21f309a92d3f847f5
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-03-02 22:44:14 -0400

    Configure session store and fix linter erros

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::SessionsController < ApplicationController
   include CurrentUserConcern
 
   def create
-    user = User.find_by(email: params["user"]["email"]).try(:authenticate,
-    params["user"]["password"])
+    user = User.find_by(email: params['user']['email']).try(:authenticate,
+                                                            params['user']['password'])
 
     if user
       session[:user_id] = user.id
